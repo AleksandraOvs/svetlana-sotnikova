2b3de63b4d847847a50302826536ba632f4fdbf8
aleksandra <aleksandra.ovsia@gmail.com>
2026-09-24 17:52:34 +0300

    eds

--- wp-content/themes/svet-theme/sections/hero-block.php
+++ wp-content/themes/svet-theme/sections/hero-block.php
@@ -6,6 +6,25 @@
     $buttons = carbon_get_post_meta(get_the_ID(), 'hero_button');
     $background = carbon_get_post_meta(get_the_ID(), 'hero_background');
     ?>
+    <svg
+        viewBox="0 0 1200 160"
+        preserveAspectRatio="none"
+        className={`pointer-events-none absolute left-0 top-0 w-full ${className}`}
+        aria-hidden="true">
+        <g fill="#a0000b">
+            <path d="M80,0 L80,110 C80,120 74,120 74,110 L74,0 Z" />
+            <path d="M110,0 L110,70 C110,78 105,78 105,70 L105,0 Z" />
+            <path d="M140,0 L140,140 C140,150 134,150 134,140 L134,0 Z" />
+            <path d="M320,0 L320,90 C320,100 314,100 314,90 L314,0 Z" />
+            <path d="M350,0 L350,40 C350,48 345,48 345,40 L345,0 Z" />
+            <path d="M600,0 L600,130 C600,140 594,140 594,130 L594,0 Z" />
+            <path d="M630,0 L630,60 C630,68 625,68 625,60 L625,0 Z" />
+            <path d="M920,0 L920,110 C920,120 914,120 914,110 L914,0 Z" />
+            <path d="M950,0 L950,50 C950,58 945,58 945,50 L945,0 Z" />
+            <path d="M1080,0 L1080,95 C1080,105 1074,105 1074,95 L1074,0 Z" />
+            <path d="M1110,0 L1110,30 C1110,38 1105,38 1105,30 L1105,0 Z" />
+        </g>
+    </svg>
 
     <div class="hero__inner">
         <div class="hero__inner__content">
